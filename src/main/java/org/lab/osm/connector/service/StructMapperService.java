ab695643a27c930d3b4265e87b2e6d6f24b1dec1
lab.cabrera@gmail.com <lab.cabrera@gmail.com>
2018-06-27 12:28:28 +0100

    Readme + javadoc

--- src/main/java/org/lab/osm/connector/service/StructMapperService.java
+++ src/main/java/org/lab/osm/connector/service/StructMapperService.java
@@ -2,7 +2,19 @@ package org.lab.osm.connector.service;
 
 import org.lab.osm.connector.mapper.StructMapper;
 
+/**
+ * Interface used to obtain the StructMapper associated with a given entity.
+ *
+ * @author lab.cabrera@gmail.com
+ * @since 1.0.0
+ */
 public interface StructMapperService {
 
+	/**
+	 * Gets the StructMapper used to do the type conversions of an mapped entity.
+	 * 
+	 * @param mappedClass Entity class
+	 * @return StructMapper
+	 */
 	<T> StructMapper<T> mapper(Class<T> mappedClass);
 }

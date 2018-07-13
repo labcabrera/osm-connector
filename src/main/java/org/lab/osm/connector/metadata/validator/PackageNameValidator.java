56b1a999028cd2b7d319af106ea59a9d5db616cb
lab.cabrera@gmail.com <lab.cabrera@gmail.com>
2018-07-13 11:23:08 +0100

    JUnit

---
+++ src/main/java/org/lab/osm/connector/metadata/validator/PackageNameValidator.java
@@ -0,0 +1,15 @@
+package org.lab.osm.connector.metadata.validator;
+
+import java.util.function.Function;
+import java.util.regex.Pattern;
+
+public class PackageNameValidator implements Function<String, Boolean> {
+
+	private static final String EXP = "^[a-zA-Z][\\w\\d]*(\\.[a-zA-Z][\\w\\d]*)*$";
+
+	@Override
+	public Boolean apply(String packageName) {
+		return Pattern.compile(EXP).matcher(packageName).matches();
+	}
+
+}
---
+++ src/test/java/org/lab/osm/connector/metadata/validator/PackageNameValidatorTest.java
@@ -0,0 +1,53 @@
+package org.lab.osm.connector.metadata.validator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PackageNameValidatorTest {
+
+	@Test
+	public void test_ok_01() {
+		Assert.assertTrue(new PackageNameValidator().apply("org.lab.osm"));
+	}
+
+	@Test
+	public void test_ok_02() {
+		Assert.assertTrue(new PackageNameValidator().apply("org.lab.osm"));
+	}
+
+	@Test
+	public void test_ok_03() {
+		Assert.assertTrue(new PackageNameValidator().apply("org.lab21.osm42"));
+	}
+
+	@Test
+	public void test_ok_04() {
+		Assert.assertTrue(new PackageNameValidator().apply("a.b.c"));
+	}
+
+	@Test
+	public void test_ok_05() {
+		Assert.assertTrue(new PackageNameValidator().apply("package"));
+	}
+
+	@Test
+	public void test_ko_01() {
+		Assert.assertFalse(new PackageNameValidator().apply(".org.lab.osm"));
+	}
+
+	@Test
+	public void test_ko_02() {
+		Assert.assertFalse(new PackageNameValidator().apply("org.lab.osm.."));
+	}
+
+	@Test
+	public void test_ko_03() {
+		Assert.assertFalse(new PackageNameValidator().apply("org..lab.osm"));
+	}
+
+	@Test
+	public void test_ko_04() {
+		Assert.assertFalse(new PackageNameValidator().apply("org.1.osm"));
+	}
+
+}
